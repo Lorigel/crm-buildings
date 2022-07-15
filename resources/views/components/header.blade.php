615c5d1f3019a5a1d98321a43732a1670cc26c0d
Lorigela <lorigela@gmail.com>
2022-07-15 17:40:27 +0200

    removing not needed

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,10 +1,8 @@
   <head>
-        
         <meta charset="utf-8" />
         <title>Dashboard | Ecosisma Bonus - Responsive Bootstrap 4 Admin Dashboard</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-        <meta content="Xhoen Guzja" name="author" />
         <!-- App favicon -->
         <link rel="shortcut icon" href="assets/images/favicon.ico">
 
@@ -15,7 +13,7 @@
         <link href="assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css" />
 
         <!-- Responsive datatable examples -->
-        <link href="assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css" rel="stylesheet" type="text/css" />  
+        <link href="assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css" rel="stylesheet" type="text/css" />
 
         <!-- Bootstrap Css -->
         <link href="assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
